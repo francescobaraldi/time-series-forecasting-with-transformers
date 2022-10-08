ff79c44d2deb119542b0142807475ae4fcfaaa04
Francesco Baraldi <francesco_baraldi@icloud.com>
2022-10-08 18:41:13 +0200

    modified test evaluation

--- test.py
+++ test.py
@@ -20,35 +20,33 @@ def test_singlestep(device, model, dl, forecast_len, scaler, max_num=50, save_pa
             class_idx = class_idx[0].item()
             src = input[:, :-1, :]
             src, trg = src.to(device), trg.to(device)
-            _, window_len, input_size = src.shape
-            prediction = torch.zeros((window_len + forecast_len - 1, input_size))
+            batch_size, window_len, input_size = src.shape
+            prediction = torch.zeros((batch_size, window_len + forecast_len - 1, input_size))
             first = True
             current_src = src
             for i in range(forecast_len):
                 out = model(current_src)
                 if first:
-                    prediction[:window_len, :] = out[0, :, :]
+                    prediction[:, :window_len, :] = out
                     first = False
                 else:
-                    prediction[window_len + i - 1:, :] = out[0, -1, :]
+                    prediction[:, window_len + i - 1:, :] = out[:, -1:, :]
                 
                 current_src = torch.cat((current_src[:, 1:, :], out[:, -1:, :]), dim=1)
                 
-            src_eval = src[0, :, :].clone().cpu()
-            trg_eval = trg[0, -forecast_len:, :].clone().cpu()
+            src_eval = src.clone().cpu()
+            trg_eval = trg[:, -forecast_len:, :].clone().cpu()
             prediction_eval = prediction.clone().cpu()
-            src_eval = scaler.inverse_transform(src_eval)[:, class_idx]
-            trg_eval = scaler.inverse_transform(trg_eval)[:, class_idx]
-            prediction_eval = scaler.inverse_transform(prediction_eval)[:, class_idx]
-            src_eval = torch.from_numpy(src_eval)
-            trg_eval = torch.from_numpy(trg_eval)
-            prediction_eval = torch.from_numpy(prediction_eval)
-            src_mae += torch.mean(torch.abs(src_eval[1:] - prediction_eval[:window_len - 1]))
-            src_mape += torch.mean(torch.abs((src_eval[1:] - prediction_eval[:window_len - 1]) / src_eval[1:]))
-            trg_mae += torch.mean(torch.abs(trg_eval - prediction_eval[-forecast_len:]))
-            trg_mape += torch.mean(torch.abs((trg_eval - prediction_eval[-forecast_len:]) / trg_eval))
+            for b in range(batch_size):
+                src_eval[b, :, :] = torch.from_numpy(scaler.inverse_transform(src_eval[b, :, :]))
+                trg_eval[b, :, :] = torch.from_numpy(scaler.inverse_transform(trg_eval[b, :, :]))
+                prediction_eval[b, :, :] = torch.from_numpy(scaler.inverse_transform(prediction_eval[b, :, :]))
+            src_mae += torch.mean(torch.abs(src_eval[:, 1:, :] - prediction_eval[:, :window_len - 1, :]))
+            src_mape += torch.mean(torch.abs((src_eval[:, 1:, :] - prediction_eval[:, :window_len - 1, :]) / src_eval[:, 1:, :]))
+            trg_mae += torch.mean(torch.abs(trg_eval - prediction_eval[:, -forecast_len:, :]))
+            trg_mape += torch.mean(torch.abs((trg_eval - prediction_eval[:, -forecast_len:, :]) / trg_eval))
             
-            plot_predictions(src[0:1, :, :].cpu(), trg[0:1, -forecast_len:, :].cpu(), prediction.unsqueeze(0).cpu(), scaler,
+            plot_predictions(src[0:1, :, :].cpu(), trg[0:1, -forecast_len:, :].cpu(), prediction[0:1, :, :].cpu(), scaler,
                               forecast_len, class_idx, j, save_path)
         
         src_mae /= count
@@ -75,24 +73,22 @@ def test_multistep(device, model, dl, forecast_len, scaler, max_num=50, save_pat
             
             count += 1
             src = input[:, :-1, :]
-            _, window_len, input_size = src.shape
+            batch_size, window_len, input_size = src.shape
             class_idx = class_idx[0].item()
             src, trg = src.to(device), trg.to(device)
             out = model(src)
             
-            src_eval = src[0, :, :].clone().cpu()
-            trg_eval = trg[0, -forecast_len:, :].clone().cpu()
-            prediction_eval = out[0, :, :].clone().cpu()
-            src_eval = scaler.inverse_transform(src_eval)[:, class_idx]
-            trg_eval = scaler.inverse_transform(trg_eval)[:, class_idx]
-            prediction_eval = scaler.inverse_transform(prediction_eval + torch.zeros((window_len, input_size)))[:, class_idx]
-            src_eval = torch.from_numpy(src_eval)
-            trg_eval = torch.from_numpy(trg_eval)
-            prediction_eval = torch.from_numpy(prediction_eval)
-            src_mae += torch.mean(torch.abs(src_eval[forecast_len:] - prediction_eval[:window_len - forecast_len]))
-            src_mape += torch.mean(torch.abs((src_eval[forecast_len:] - prediction_eval[:window_len - forecast_len]) / src_eval[forecast_len:]))
-            trg_mae += torch.mean(torch.abs(trg_eval - prediction_eval[-forecast_len:]))
-            trg_mape += torch.mean(torch.abs((trg_eval - prediction_eval[-forecast_len:]) / trg_eval))
+            src_eval = src.clone().cpu()
+            trg_eval = trg[:, -forecast_len:, :].clone().cpu()
+            prediction_eval = out.clone().cpu()
+            for b in range(batch_size):
+                src_eval[b, :, :] = torch.from_numpy(scaler.inverse_transform(src_eval[b, :, :]))
+                trg_eval[b, :, :] = torch.from_numpy(scaler.inverse_transform(trg_eval[b, :, :]))
+                prediction_eval[b, :, :] = torch.from_numpy(scaler.inverse_transform(prediction_eval[b, :, :]))
+            src_mae += torch.mean(torch.abs(src_eval[:, forecast_len:, :] - prediction_eval[:, :window_len - forecast_len, :]))
+            src_mape += torch.mean(torch.abs((src_eval[:, forecast_len:, :] - prediction_eval[:, :window_len - forecast_len, :]) / src_eval[:, forecast_len:, :]))
+            trg_mae += torch.mean(torch.abs(trg_eval - prediction_eval[:, -forecast_len:, :]))
+            trg_mape += torch.mean(torch.abs((trg_eval - prediction_eval[:, -forecast_len:, :]) / trg_eval))
             
             plot_predictions(src[0:1, :, :].cpu(), trg[0:1, -forecast_len:, :].cpu(), out[0:1, :, :].cpu(), scaler,
                              forecast_len, class_idx, j, save_path)
