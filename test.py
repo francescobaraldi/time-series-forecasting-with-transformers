af5c5a08b1e1d49a61563d6e4f8daef603e9eaea
Francesco Baraldi <francesco_baraldi@icloud.com>
2022-10-20 12:49:38 +0200

    added multistep for transformer

--- test.py
+++ test.py
@@ -81,6 +81,41 @@ def test_transformer(device, model, dl, forecast_len, scaler, max_num=50, save_p
         mape /= count
         
         return mae, mape
+    
+
+def test_transformer_multistep(device, model, dl, forecast_len, scaler, max_num=50, save_path=None):
+    model = model.to(device)
+    model.eval()
+    
+    with torch.no_grad():
+        mae = 0
+        mape = 0
+        count = 0
+        for j, (input, window_len, class_idx) in enumerate(dl):
+            if j >= max_num:
+                break
+            
+            count += 1
+            window_len = window_len[0].item()
+            class_idx = class_idx[0].item()
+            input = input.to(device)
+            src = input[:, :window_len, :]
+            trg = input[:, window_len - 1:window_len - 1 + forecast_len, :]
+            trg_y = input[:, -forecast_len:, :]
+            batch_size, _, input_size = src.shape
+            prediction = torch.zeros((batch_size, forecast_len, input_size)).to(device)
+            out = model(src, trg)
+            
+            mae += torch.mean(torch.abs(trg_y - out))
+            mape += torch.mean(torch.abs((trg_y - out) / trg_y))
+            
+            plot_predictions(src[0:1, :, :].cpu(), trg_y[0:1, :, :].cpu(), prediction[0:1, :, :].cpu(), scaler, forecast_len, class_idx, j,
+                             save_path)
+        
+        mae /= count
+        mape /= count
+        
+        return mae, mape
 
 
 def test_lstm(device, model, dl, forecast_len, scaler, max_num=50, save_path=None):
